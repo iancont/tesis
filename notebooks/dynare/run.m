6060cf10c1a554c33b4179fa29eb3f8e0ca487a3
Ian Contreras <contreras.mejia@hotmail.com>
2025-01-04 00:01:35 -0400

    welfare loss analysis done

---
+++ notebooks/dynare/run.m
@@ -0,0 +1,14 @@
+clear all; close all; clc;
+
+addpath /usr/lib/dynare/matlab/
+
+% dynare nash
+% irfs_nash = oo_.irfs;
+% close all;
+
+% dynare fiscal
+% irfs_fiscal = oo_.irfs;
+% close all;
+
+% dynare monetary
+% close all;
